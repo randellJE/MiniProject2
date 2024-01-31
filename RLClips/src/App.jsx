e0a535d530b8211ff8a38b0a8857c6897198c2ea
randellJE <randellokelly@gmail.com>
2024-01-31 17:05:19 -0500

    Updated code to form skeleton for website

---
+++ RLClips/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Route, Routes} from "react-router-dom"
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path='/' element={<LoginPage />}  />
+          <Route path='/register' element={<RegisterPage />}  />
+          <Route path='/home' element={<HomePage />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
